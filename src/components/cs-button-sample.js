659e92f33376c2f383e63d150adedf22ef6a9bd7
Eben <me@ebenroux.co.za>
2017-11-22 19:43:38 +0200

    - button

--- src/components/cs-button-sample.js
+++ src/components/cs-button-sample.js
@@ -1,14 +1,38 @@
 import DefineMap from 'can-define/map/';
 import Component from 'can-component';
 import view from './cs-button-sample.stache!';
-import {ActionItem,ActionItemList} from 'shuttle-canstrap/button/';
+import {ActionItem, ActionItemList} from 'shuttle-canstrap/button/';
+
+var handler = function(message){
+    alert(message);
+}
 
 var ViewModel = DefineMap.extend({
-  actions: { Type: ActionItemList }
+    actions: {Type: ActionItemList},
+    click: function (ev) {
+        handler('click');
+    }
 });
 
 export default Component.extend({
     tag: 'cs-button-sample',
     view,
-    ViewModel
+    ViewModel: function () {
+        return new ViewModel({
+            actions: [
+                {
+                    text: "action-1 (with click)",
+                    click: function(){
+                        handler('action-1 click');
+                    }
+                },
+                {
+                    text: "action-2"
+                },
+                {
+                    text: "action-3"
+                },
+            ]
+        })
+    }
 });
