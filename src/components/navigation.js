ee9c1e4602750d6dd5dcbea296dd104997f0dee7
Eben <me@ebenroux.co.za>
2017-12-08 21:38:52 +0200

    - updated v1.0.8 / nav-dropdown

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,33 +1,30 @@
 import DefineMap from 'can-define/map/';
-import DefineList from 'can-define/list/';
 import Component from 'can-component';
 import view from './navigation.stache!';
 import each from 'can-util/js/each/';
-import {DropdownMap,DropdownList} from 'shuttle-canstrap/nav-dropdown/';
+import {DropdownMap, DropdownList} from 'shuttle-canstrap/nav-dropdown/';
 import route from 'can-route/';
 
 var ViewModel = DefineMap.extend({
-  resources: { 
-    get: function(value) {
-      var result = new DropdownList();
-      
-      var dropdown = new DropdownMap({
-        text: 'cs-nav-dropdown',
-        items: new DropdownList()
-      });
-      
-      each(value, function(resource) {
-        dropdown.items.push({
-          text: resource.name,
-          href: route.url({resource: resource.name})
-        });
-      });
-      
-      result.push(dropdown);
-      
-      return result;
+    map: {
+        value: {
+            text: 'cs-nav-dropdown'
+        }
+    },
+    list: {
+        get: function (value) {
+            var result = new DropdownList();
+
+            each(value, function (resource) {
+                result.push({
+                    text: resource.name,
+                    href: route.url({resource: resource.name})
+                });
+            });
+
+            return result;
+        }
     }
-  }
 });
 
 export default Component.extend({
